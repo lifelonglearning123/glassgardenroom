3ec0881c0f8c5caead0c83cc35ceba315b7e5dc4
chao liu <chao@macaws.ai>
2026-06-27 19:51:07 +0100

    Revert "Add Sanity CMS migration: Astro site + Studio + seed"
    This reverts commit 2ef7356010c3388c67e80d7754ceb8b722d88814.

--- studio/schemas/objects/index.ts
+++
@@ -1,7 +0,0 @@
-export { cta } from './cta';
-export { imageWithCaption } from './imageWithCaption';
-export { gallery } from './gallery';
-export { seo } from './seo';
-export { richText } from './richText';
-export { editorialBlock } from './editorialBlock';
-export { momentBlock } from './momentBlock';
